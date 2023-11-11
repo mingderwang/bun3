1f0b5b405f4f2ded0596656908f918c42300766e
Ming-der Wang <mingderwang@gmail.com>
2023-11-11 22:51:34 +0800

    change port to 3001

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
-Bun.serve({
+const server = Bun.serve({
+    port: 3001,
     fetch(req: Request) {
       const res: Response= new Response(JSON.stringify(process.env));    
       res.headers.set('Content-Type', 'application/json');
       return res;
     },
   });
+
+  console.log(`Listening on http://localhost:${server.port} ...`);
